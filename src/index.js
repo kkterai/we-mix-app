ae3736f1ec9f6ca71534913ec2c819676a6e7bce
kkterai <kkterai@gmail.com>
2017-10-12 15:45:42 -0700

    create app, video, and videos folder

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App'
 
- 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        We-Mix!!
-      </div>
-    );
-  }
-}
-
-// ========================================
 
 ReactDOM.render(
   <App/>,
