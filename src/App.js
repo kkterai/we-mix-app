ef17bc8c8a261d4eb849dc1ad0043f5c70b5b86e
kkterai <kkterai@gmail.com>
2017-10-12 17:03:48 -0700

    added redux, react-redux, thunk, and whatwg-fetch to dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            videos: []
+        }
+
+    }
+
+    componentWillMount() {
+        this.getUserVideos();
+    }
+
+    getUserVideos = () => {
+        let userVideos = fetch('http://localhost:3000/api/v1/videos').
+        then(res => res.json()).
+        then(json => console.log(json))
+        this.setState({ 
+            videos: userVideos
+        });
+    }
+
+    // componentDidMount - Music API
+
+
     render() {
+        debugger
       return (
         <div className="app">
-          We-Mix!!
+          We-Mix!!!
         </div>
       );
     }
