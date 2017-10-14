fb1434fd256d635ab451217432e4d81c06fc047b
kkterai <kkterai@gmail.com>
2017-10-13 22:48:34 -0700

    attempt to use CORS proxy

--- src/actions/videoActions.js
+++ src/actions/videoActions.js
@@ -2,9 +2,11 @@ require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 export function getUserVideos() {
+  const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+  const url = 'http://localhost:3000/api/v1/videos';
   return (dispatch) => {
     dispatch({ type: 'LOAD_USER_VIDEOS' });
-    return fetch('http://localhost:3000/api/v1/videos')
+    return fetch(proxyurl + url)
       .then(response => response.json())
       .then(videos => dispatch({ type: 'FETCH_USER_VIDEOS', payload: videos }));
   };
