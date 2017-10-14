0def36326b300a2cfea6b18603df525bb10818cc
kkterai <kkterai@gmail.com>
2017-10-14 14:19:30 -0700

    stub out musikki code

--- src/actions/videoActions.js
+++ src/actions/videoActions.js
@@ -22,3 +22,17 @@ export function searchArtist(name) {
         .then(artist => dispatch({ type: 'FETCH_ARTIST_VIDEOS', payload: artist }));
     };
 }
+
+
+// export function searchArtist(name) {
+//   const key = process.env.REACT_APP_MUSIKKI_APP_KEY 
+//   const id = process.env.REACT_APP_MUSIKKI_APP_ID
+//   const corsURL = 'https://cors-anywhere.herokuapp.com/'
+//   let artist = name
+//   return (dispatch) => {
+//     dispatch({ type: 'SEARCH_ARTIST_VIDEOS' });
+//     return fetch(`${corsURL}https://music-api.musikki.com/v1/artists?q=[artist-name:${artist}]&appkey=your_key&appid=${secret}`)
+//         .then(response => response.json())
+//         .then(artist => dispatch({ type: 'FETCH_ARTIST_VIDEOS', payload: artist }));
+//     };
+// }
