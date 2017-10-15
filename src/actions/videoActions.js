73598e9a0a82dc219dd93ba44eb974ded84ab61e
kkterai <kkterai@gmail.com>
2017-10-14 23:11:28 -0700

    on second thought, back to AudioDB

--- src/actions/videoActions.js
+++ src/actions/videoActions.js
@@ -12,13 +12,11 @@ export function getUserVideos() {
 }
 
 export function searchArtist(name) {
-  const key = process.env.REACT_APP_KEY
-  const id = process.env.REACT_APP_ID
+  const secret = process.env.REACT_APP_SECRET_CODE
   const corsURL = 'https://cors-anywhere.herokuapp.com/'
-  let artist = name
   return (dispatch) => {
     dispatch({ type: 'SEARCH_ARTIST_VIDEOS' });
-    return fetch(`${corsURL}https://music-api.musikki.com/v1/artists?q=[artist-name:${artist}]&appkey=${key}&appid=${id}`)
+    return fetch(`${corsURL}http://www.theaudiodb.com/api/v1/json/${secret}/searchalbum.php?s=${name}`)
         .then(response => response.json())
         .then(artist => dispatch({ type: 'FETCH_ARTIST_VIDEOS', payload: artist }));
     };
