afd094f27e97945df356e91ee2282081db3ea33c
kkterai <kkterai@gmail.com>
2017-11-06 21:36:03 -0800

    added local storage

--- src/index.js
+++ src/index.js
@@ -7,13 +7,20 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle';
 
-
+const persistedState = loadState();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+const store = createStore(rootReducer, persistedState, composeEnhancers(
     applyMiddleware(thunk)
   ));
+
+store.subscribe(throttle(() => {
+  saveState({
+    videos: store.getState().videos //confirm that this will preserve data, not ui state 
+  });
+}, 1000));
 
 ReactDOM.render(
   <Provider store={store} >
