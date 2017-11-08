f0435fe1cdc41e34a30954b40aa279b9567f5dea
kkterai <kkterai@gmail.com>
2017-11-07 16:16:07 -0800

    add SessionApi class

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS'  
--- src/actions/sessionActions.js
+++ src/actions/sessionActions.js
@@ -1,5 +1,5 @@
 import * as types from './actionTypes';
-import auth from '../auth/authenticator';
+import sessionApi from '../api/SessionApi';
 
 // example ref: sbenedetto
 
